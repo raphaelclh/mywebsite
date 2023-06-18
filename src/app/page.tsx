af8d7c3e98db7e1604fc8e93720fd296081ebb8b
raphael <raphaelvguy@gmail.com>
2023-06-18 23:38:16 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import {
   Container,
   Heading,
   Text,
+  Image,
+  Flex,
 } from "@chakra-ui/react";
 
 import Section from "../components/section";
@@ -30,8 +32,22 @@ const Home = () => {
         </Text>
       </Section>
       <Section bg="palette.200">
-        <Heading>Hi. I am Raphael</Heading>
-        <Text>Software Engineer in Singapore</Text>
+        <Flex h={16} alignItems={"center"} justifyContent={"center"}>
+          <Box p={10}>
+            <Text
+              textAlign={"center"}
+              fontSize={{ base: "10px", sm: "10px", md: "14px", lg: "16px" }}
+            >
+              Software Engineer in Singapore
+            </Text>
+          </Box>
+          <Image
+            borderRadius="full"
+            boxSize="150px"
+            src="https://bit.ly/dan-abramov"
+            alt="Dan Abramov"
+          />
+        </Flex>
       </Section>
       <Section>
         <Heading>Hi. I am Raphael</Heading>
