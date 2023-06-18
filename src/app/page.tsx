2e498760f334119d33769f0bfae6715ae09eb4b1
raphael <raphaelvguy@gmail.com>
2023-06-18 17:48:35 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,12 @@ import Section from "../components/section";
 const Home = () => {
   const bg = useColorModeValue("brand.500", "teal");
   return (
-    <Container centerContent>
+    <Box>
       <Section>
+        <Heading>Hi. I'm Raphael</Heading>
+        <Text textAlign={"center"}>Software Engineer in Singapore</Text>
+      </Section>
+      <Section bg="palette.200">
         <Heading>Hi. I am Raphael</Heading>
         <Text>Software Engineer in Singapore</Text>
       </Section>
@@ -23,11 +27,7 @@ const Home = () => {
         <Heading>Hi. I am Raphael</Heading>
         <Text>Software Engineer in Singapore</Text>
       </Section>
-      <Section>
-        <Heading>Hi. I am Raphael</Heading>
-        <Text>Software Engineer in Singapore</Text>
-      </Section>
-    </Container>
+    </Box>
   );
 };
 
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,9 +1,16 @@
 import { AbsoluteCenter, Box } from "@chakra-ui/react";
 import React from "react";
 
-const section = ({ children, ...props }: { children: React.ReactNode }) => {
+const section = ({
+  bg = "palette.100",
+  children,
+  ...props
+}: {
+  bg?: string;
+  children: React.ReactNode;
+}) => {
   return (
-    <Box position="relative" h="100vh" w="100vw">
+    <Box position="relative" h="100vh" w="100vw" maxW="100%" bg={bg}>
       <AbsoluteCenter p="4" color="white" axis="both">
         {children}
       </AbsoluteCenter>
