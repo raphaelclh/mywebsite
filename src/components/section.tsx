13a1793becc6bfbb8b8c8c37e0720817cda45180
raphael <raphaelvguy@gmail.com>
2023-06-18 18:03:10 +0800

    update

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -4,13 +4,12 @@ import React from "react";
 const section = ({
   bg = "palette.100",
   children,
-  ...props
 }: {
   bg?: string;
   children: React.ReactNode;
 }) => {
   return (
-    <Box position="relative" h="100vh" w="100vw" maxW="100%" bg={bg} {...props}>
+    <Box position="relative" h="100vh" w="100vw" maxW="100%" bg={bg}>
       <AbsoluteCenter p="4" color="white" axis="both">
         {children}
       </AbsoluteCenter>
