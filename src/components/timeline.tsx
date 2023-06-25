015727bec45df29192f2541d57882768fb2afcb6
raphael <raphaelvguy@gmail.com>
2023-06-25 20:50:55 +0800

    updates

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -4,62 +4,142 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import "./generic.css";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { MdOutlineWork } from "react-icons/md";
+import { Text, UnorderedList, ListItem } from "@chakra-ui/react";
 
 const Timeline = () => {
+  const textWithoutMargin = { margin: "0 auto !important" };
   return (
     <VerticalTimeline>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
         contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
         contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-        date="2011 - present"
+        date="Aug 2022 - Present"
+        dateClassName="timeline-date"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<FaGithub />}
+        icon={<MdOutlineWork />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project
-          Management, Team Leading
-        </p>
+        <Text
+          sx={textWithoutMargin}
+          color={"#FFF"}
+          fontWeight={"bold !important"}
+        >
+          Developer @ Standard Chartered
+        </Text>
+        <Text sx={textWithoutMargin} color={"#FFF"}>
+          Singapore
+        </Text>
+
+        <UnorderedList>
+          <ListItem>
+            <Text color={"#FFF"}>
+              Developed UI application for elasticsearch API with ReactJS and
+              NodeJS.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin} color={"#FFF"}>
+              Worked with helm chart tool to deploy the application to Amazon
+              EKS.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin} color={"#FFF"}>
+              Worked on the proof of concept for migrating of pipeline
+              strategies with Azure Devops tools.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin} color={"#FFF"}>
+              Worked on the Continuous Deployment templates for Azure Devops
+              which were successfully adopted by all the teams.
+            </Text>
+          </ListItem>
+        </UnorderedList>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="2010 - 2011"
+        date="May 2021 - July 2022"
+        dateClassName="timeline-date"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<FaGithub />}
+        icon={<MdOutlineWork />}
       >
-        <h3 className="vertical-timeline-element-title">Art Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          San Francisco, CA
-        </h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, SEO, Online
-          Marketing
-        </p>
+        <Text sx={textWithoutMargin} fontWeight={"bold !important"}>
+          Software Engineer @ Envision Digital International
+        </Text>
+        <Text sx={textWithoutMargin}>Singapore</Text>
+
+        <UnorderedList>
+          <ListItem>
+            <Text>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+        </UnorderedList>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="2008 - 2010"
+        date="July 2020 - May 2021"
+        dateClassName="timeline-date"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<FaGithub />}
+        icon={<MdOutlineWork />}
       >
-        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-        <p>User Experience, Visual Design</p>
+        <Text sx={textWithoutMargin} fontWeight={"bold !important"}>
+          Consultant @ NTT Ltd.
+        </Text>
+        <Text sx={textWithoutMargin}>Singapore</Text>
+
+        <UnorderedList>
+          <ListItem>
+            <Text>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+        </UnorderedList>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="2006 - 2008"
+        date="July 2019 - June 2020"
+        dateClassName="timeline-date"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<FaGithub />}
+        icon={<MdOutlineWork />}
       >
-        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          San Francisco, CA
-        </h4>
-        <p>User Experience, Visual Design</p>
+        <Text sx={textWithoutMargin} fontWeight={"bold !important"}>
+          Cloud Engineer @ Aviva Asia
+        </Text>
+        <Text sx={textWithoutMargin}>Singapore</Text>
+
+        <UnorderedList>
+          <ListItem>
+            <Text>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+          </ListItem>
+        </UnorderedList>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--education"
