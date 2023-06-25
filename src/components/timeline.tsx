95a7f83abf9047afb526cadf254b5783067ce59c
raphael <raphaelvguy@gmail.com>
2023-06-25 21:34:17 +0800

    updates

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -15,47 +15,41 @@ const Timeline = () => {
     <VerticalTimeline>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+        // contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        // contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
         date="Aug 2022 - Present"
         dateClassName="timeline-date"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
         icon={<MdOutlineWork />}
       >
-        <Text
-          sx={textWithoutMargin}
-          color={"#FFF"}
-          fontWeight={"bold !important"}
-        >
+        <Text sx={textWithoutMargin} fontWeight={"bold !important"}>
           Developer @ Standard Chartered
         </Text>
-        <Text sx={textWithoutMargin} color={"#FFF"}>
-          Singapore
-        </Text>
+        <Text sx={textWithoutMargin}>Singapore</Text>
 
         <UnorderedList>
           <ListItem>
-            <Text color={"#FFF"}>
+            <Text>
               Developed UI application for elasticsearch API with ReactJS and
-              NodeJS.
+              NodeJS
             </Text>
           </ListItem>
           <ListItem>
-            <Text sx={textWithoutMargin} color={"#FFF"}>
+            <Text sx={textWithoutMargin}>
               Worked with helm chart tool to deploy the application to Amazon
-              EKS.
+              EKS
             </Text>
           </ListItem>
           <ListItem>
-            <Text sx={textWithoutMargin} color={"#FFF"}>
+            <Text sx={textWithoutMargin}>
               Worked on the proof of concept for migrating of pipeline
-              strategies with Azure Devops tools.
+              strategies with Azure Devops tools
             </Text>
           </ListItem>
           <ListItem>
-            <Text sx={textWithoutMargin} color={"#FFF"}>
+            <Text sx={textWithoutMargin}>
               Worked on the Continuous Deployment templates for Azure Devops
-              which were successfully adopted by all the teams.
+              which were successfully adopted by all the teams
             </Text>
           </ListItem>
         </UnorderedList>
@@ -68,22 +62,59 @@ const Timeline = () => {
         icon={<MdOutlineWork />}
       >
         <Text sx={textWithoutMargin} fontWeight={"bold !important"}>
-          Software Engineer @ Envision Digital International
+          Software Engineer @ Envision Digital
         </Text>
         <Text sx={textWithoutMargin}>Singapore</Text>
 
         <UnorderedList>
           <ListItem>
-            <Text>Lorem ipsum dolor sit amet</Text>
+            <Text>
+              Implementing unit testing for various react components using Jest
+              and React-Testing libraries
+            </Text>
           </ListItem>
           <ListItem>
-            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+            <Text sx={textWithoutMargin}>
+              Responsible for handling multiple change requests in the front-end
+            </Text>
           </ListItem>
           <ListItem>
-            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+            <Text sx={textWithoutMargin}>
+              Build various react components in typescript using envision&apos;s
+              custom libraries
+            </Text>
+          </ListItem>
+          <UnorderedList pl={4}>
+            <ListItem>
+              <Text sx={textWithoutMargin}>
+                Data visualization and forecast graphs
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text sx={textWithoutMargin}>
+                Building Alert dashboard to monitor IoT devices
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text sx={textWithoutMargin}>
+                Configuration web page for recommender system
+              </Text>
+            </ListItem>
+          </UnorderedList>
+          <ListItem>
+            <Text sx={textWithoutMargin}>
+              Provide bug fixes support in web application
+            </Text>
           </ListItem>
           <ListItem>
-            <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
+            <Text sx={textWithoutMargin}>
+              Contribute to Envision UI library
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text sx={textWithoutMargin}>
+              Responsible for deployment of various services in the pipeline
+            </Text>
           </ListItem>
         </UnorderedList>
       </VerticalTimelineElement>
@@ -107,6 +138,10 @@ const Timeline = () => {
             <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
           </ListItem>
           <ListItem>
+            <UnorderedList pl={4}>
+              <ListItem>Second-level item 1</ListItem>
+              <ListItem>Second-level item 2</ListItem>
+            </UnorderedList>
             <Text sx={textWithoutMargin}>Lorem ipsum dolor sit amet</Text>
           </ListItem>
           <ListItem>
